dfc71106fb6438d4eee6ad82d7e4c04882446be3
micheal1woo <kjw8922@naver.com>
2025-02-02 16:37:38 +0900

    제목변경 및 파일수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <h1>안녕 리액트!</h1>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <div>
+  <h1 id="title">가위바위보</h1>
+  <button className="hand">가위</button> 
+  <button className="hand">바위</button>
+ <button className="hand">보</button>
+ </div>
 );
